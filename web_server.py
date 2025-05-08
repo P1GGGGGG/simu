35b91d2a1e5ceacee020e81d68b54ba3aa817427
P1GGGGGG <zyl2152715701@163.com>
2025-05-08 20:12:50 +0800

    Earl Zhu : 添加web后端数据，记录无人机资源与位置信息

--- web_server.py
+++ web_server.py
@@ -4,9 +4,6 @@ import time
 from datetime import datetime
 from typing import Dict, List, Set, Optional
 import websockets
-# from internal.UAV_system.uavs_init import drones
-# from internal.task_system.task_init import tasks
-# from internal.airsim_server.uavsGetTask import assignments
 import resource_gen
 
 class EnhancedDroneCoordinator:
@@ -26,35 +23,35 @@ class EnhancedDroneCoordinator:
         """从DroneDataGenerator获取无人机资源数据"""
         return self.data_generator.get_drone_data()
 
-    def _generate_progress(self) -> List[Dict]:
-        """生成符合TaskProgress接口的进度数据"""
-        progress_data = []
-        #每次对所有任务记进行遍历
-        for task in tasks:
-            # 获取执行该任务的无人机ID列表
-            assigned_drones = assignments.get("_task_index", {}).get(task.task_id, [])
-            for drone_id in assigned_drones:
-                progress_data.append({
-                    "taskId": str(task.task_id),
-                    "droneId": drone_id,
-                    "taskName": task.name,
-                    "progress": round((task.duration - task.surplus) / task.duration * 100, 1)
-                })
-        #print(progress_data)
-        return progress_data
-
-    def _calculate_statistics(self) -> Dict:
-        """计算任务统计信息"""
-        status_count = {0: 0, 1: 0, 2: 0, 3: 0}
-        for task in tasks:
-            status_count[task.status] += 1
-        #print(json.dumps(status_count))
-        return {
-            "notStarted": status_count[0],
-            "inProgress": status_count[1],
-            "completed": status_count[2],
-            "failed": status_count[3]
-        }
+    # def _generate_progress(self) -> List[Dict]:
+    #     """生成符合TaskProgress接口的进度数据"""
+    #     progress_data = []
+    #     #每次对所有任务记进行遍历
+    #     for task in tasks:
+    #         # 获取执行该任务的无人机ID列表
+    #         assigned_drones = assignments.get("_task_index", {}).get(task.task_id, [])
+    #         for drone_id in assigned_drones:
+    #             progress_data.append({
+    #                 "taskId": str(task.task_id),
+    #                 "droneId": drone_id,
+    #                 "taskName": task.name,
+    #                 "progress": round((task.duration - task.surplus) / task.duration * 100, 1)
+    #             })
+    #     #print(progress_data)
+    #     return progress_data
+    #
+    # def _calculate_statistics(self) -> Dict:
+    #     """计算任务统计信息"""
+    #     status_count = {0: 0, 1: 0, 2: 0, 3: 0}
+    #     for task in tasks:
+    #         status_count[task.status] += 1
+    #     #print(json.dumps(status_count))
+    #     return {
+    #         "notStarted": status_count[0],
+    #         "inProgress": status_count[1],
+    #         "completed": status_count[2],
+    #         "failed": status_count[3]
+    #     }
 
     def _calculate_metrics(self) -> Dict:
         """生成系统性能指标"""
@@ -63,21 +60,21 @@ class EnhancedDroneCoordinator:
             "responseTime": round((time.time() - self.last_update_time) * 1000, 1)
         }
 
-    def _generate_tasksResource(self) -> List[Dict]:
-        Tstatus_map = {
-            0: 'noStart',
-            1: 'assigned',
-            2: 'running',
-            3: 'breaking',
-            4: 'end'
-        }
-        """生成任务数据"""
-        return [{
-            "id": task.task_id,
-            "tx": task.x,
-            "ty": task.y,
-            "Tstatus": Tstatus_map.get(task.status, '未知状态')
-        } for task in tasks]
+    # def _generate_tasksResource(self) -> List[Dict]:
+    #     Tstatus_map = {
+    #         0: 'noStart',
+    #         1: 'assigned',
+    #         2: 'running',
+    #         3: 'breaking',
+    #         4: 'end'
+    #     }
+    #     """生成任务数据"""
+    #     return [{
+    #         "id": task.task_id,
+    #         "tx": task.x,
+    #         "ty": task.y,
+    #         "Tstatus": Tstatus_map.get(task.status, '未知状态')
+    #     } for task in tasks]
 
     async def _send_data(self, msg_type: str, data: any):
         """统一数据发送方法"""
@@ -99,18 +96,18 @@ class EnhancedDroneCoordinator:
             try:
                 # 生成最新数据
                 new_res = self._generate_resources()
-                new_prog = self._generate_progress()
-                new_stat = self._calculate_statistics()
+                # new_prog = self._generate_progress()
+                # new_stat = self._calculate_statistics()
                 new_met = self._calculate_metrics()
-                new_tas = self._generate_tasksResource()
+                # new_tas = self._generate_tasksResource()
                 #print(new_tas)
 
                 # 发送更新
                 await self._send_data("resources", new_res)
-                await self._send_data("progress", new_prog)
-                await self._send_data("statistics", new_stat)
+                # await self._send_data("progress", new_prog)
+                # await self._send_data("statistics", new_stat)
                 await self._send_data("metrics", new_met)
-                await self._send_data("tasksData", new_tas)
+                # await self._send_data("tasksData", new_tas)
 
                 self.last_update_time = time.time()
                 await asyncio.sleep(0.5)  # 固定500ms更新间隔
